2d1bb8ea4e78bc39f1e9c7195129f0ace9beb3c9
minghu6 <a19678zy@163.com>
2017-01-26 21:17:03 +0800

    ..

--- minghu6_test/tools/add_pypath_test.py
+++ minghu6_test/tools/add_pypath_test.py
@@ -13,11 +13,11 @@ from minghu6.etc.path import chdir
 pypath = sys.executable
 
 def test_add_pypath():
-    with chdir(__file__):
+    with chdir(os.path.dirname(__file__)):
         cmd = '{0} ../../minghu6/tools/add_pypath.py --help'.format(pypath)
         info_lines, err_lines = exec_cmd(cmd)
-        
-    print(pypath)
+
+    print(pypath, os.path.dirname(__file__))
     assert not err_lines
     assert info_lines
 
