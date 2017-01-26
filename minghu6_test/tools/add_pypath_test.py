b6f3f89f67307705af446986e8655a7745685772
minghu6 <a19678zy@163.com>
2017-01-26 21:09:01 +0800

    ..

--- minghu6_test/tools/add_pypath_test.py
+++ minghu6_test/tools/add_pypath_test.py
@@ -11,9 +11,10 @@ from minghu6.etc.cmd import exec_cmd
 pypath = sys.executable
 
 def test_add_pypath():
-    cmd = '{0} -m minghu6.tools.add_pypath --help'.format(pypath)
-    print(pypath)
-    assert os.system(cmd) == 0
+    cmd = '{0} ../../minghu6/tools/add_pypath.py --help'.format(pypath)
+    info_lines, err_lines = exec_cmd(cmd)
+    assert not err_lines
+    assert info_lines
 
 if __name__ == '__main__':
     test_add_pypath()
