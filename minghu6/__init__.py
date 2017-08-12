5923ea6f747ed458b148b4b977c359a9a6cca75b
John.Zhuang <johnzhuang@augmentum.com.cn>
2017-08-13 01:18:02 +0800

    ...

--- minghu6/__init__.py
+++ minghu6/__init__.py
@@ -4,12 +4,4 @@
 Nothing
 ################################################################################
 """
-import sys
-import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from setup import __version__ as VERSION
-
-__version__ = VERSION
-print(__version__)
