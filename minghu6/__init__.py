f8773ad827a8f3016b2eeec4b06ca4b5ec0bb12f
John.Zhuang <johnzhuang@augmentum.com.cn>
2017-08-13 00:59:20 +0800

    update setup

--- minghu6/__init__.py
+++ minghu6/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding:utf-8 -*-
-
-
 """
 ################################################################################
 Nothing
 ################################################################################
 """
-
-from ..setup import __version__ as VERSION
+import os
+os.path.join(os.path.abspath(__file__), os.pardir)
+from setup import __version__ as VERSION
 
 __version__ = VERSION
