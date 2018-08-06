71c5bcc371be84d041aedc947912728f1beed8ab
minghu6 <a19678zy@163.com>
2018-08-06 23:13:50 +0800

    update

--- minghu6/algs/func.py
+++ minghu6/algs/func.py
@@ -11,10 +11,12 @@ def chain_apply(funcs, var):
     return var
 
 
-def flatten(items):
+def flatten(items, class_type=None):
     """Yield items from any nested iterable; see REF."""
     for x in items:
-        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+        if class_type is not None and not isinstance(x, class_type):
+            yield from flatten(x)
+        elif isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
             yield from flatten(x)
         else:
             yield x
