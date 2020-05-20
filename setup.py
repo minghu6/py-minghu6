b155cf1373937d84ca3b61d74f4c09ff8eb0824b
minghu6 <a19678zy@163.com>
2020-05-20 23:51:03 +0800

    add tag v1.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 with open('requirements.txt') as f:
     REQUIRED = f.read().splitlines()
