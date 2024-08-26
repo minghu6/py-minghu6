eaa6caf822ede7475de515192ee903df4d2a0a0d
minghu6 <a19678zy@163.com>
2024-08-26 17:47:15 +0800

    "tag v1.6.4"

--- minghu6/__init__.py
+++ minghu6/__init__.py
@@ -5,7 +5,7 @@ Nothing
 """
 from os.path import abspath, dirname, join
 import hy
-__version__ = <Version('1.6.3')>
+__version__ = '1.6.4'
 MINGHU_HOME = abspath(dirname(__file__))
 RESOURCES_HOME = join(dirname(MINGHU_HOME), 'resources')
 TEMPLATES_HOME = join(RESOURCES_HOME, 'templates')
